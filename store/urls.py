d7a96958a1a405a2ab7f876c2b64ab92e9343a85
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-11-09 17:29:55 +0800

    urls add to cart remove

--- store/urls.py
+++ store/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('specific-category/<str:item_category>/', views.specific_category, name='specific-category'),
     path('cart/', views.cart, name='cart'),
     path('buy-now/<slug:item_slug>/', views.buy_now, name='buy-now'),
-    path('add-to-cart/<slug:item_slug>/', views.add_to_cart, name='add-to-cart'),
     path('increase-quantity/<slug:item_slug>/', views.increase_quantity, name='increase-quantity'),
     path('decrease-quantity/<slug:item_slug>/', views.decrease_quantity, name='decrease-quantity'),
     path('remove-product/<slug:item_slug>/', views.remove_product, name='remove-product'),
