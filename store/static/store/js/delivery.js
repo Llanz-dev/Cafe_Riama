d31b6a95fc3921a7cf9d249f8d187b5dfecb588a
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-12-17 12:58:33 +0800

    Delivery amount fee from Django to JavaScript

--- store/static/store/js/delivery.js
+++ store/static/store/js/delivery.js
@@ -4,30 +4,37 @@ let office_input = document.getElementById('office-input') ;
 let home_input = document.getElementById('home-input');
 let districts = document.getElementById('id_districts');
 let sub_total = document.getElementById('sub_total');
-let delivery_fee_amount = 70
+
 districts.onclick = () => {
+    const arevalo = document.getElementById('arevalo');
+    const city_proper = document.getElementById('city_proper');
+    const jaro = document.getElementById('jaro');
+    const la_paz = document.getElementById('la_paz');
+    const lapuz = document.getElementById('lapuz');
+    const mandurriao = document.getElementById('mandurriao');
+    const molo = document.getElementById('molo');
     const total = document.getElementById('total');
     const delivery_fee = document.getElementById('delivery_fee');
     if (districts.value === 'Arevalo') {
-        delivery_fee.textContent = 70;   
+        delivery_fee.textContent = arevalo.textContent;   
     }
     if (districts.value === 'City Proper') {
-        delivery_fee.textContent = 60;
+        delivery_fee.textContent = city_proper.textContent;
     }
     if (districts.value === 'Jaro') {
-            delivery_fee.textContent = 50;    
+        delivery_fee.textContent = jaro.textContent;    
     }
     if (districts.value === 'La Paz') {
-            delivery_fee.textContent = 60;    
+        delivery_fee.textContent = la_paz.textContent;    
     }
     if (districts.value === 'Lapuz') {
-        delivery_fee.textContent = 80;
+        delivery_fee.textContent = lapuz.textContent;
     }
     if (districts.value === 'Mandurriao') {
-        delivery_fee.textContent = 40;
+        delivery_fee.textContent = mandurriao.textContent;
     }
     if (districts.value === 'Molo') {
-        delivery_fee.textContent = 70;
+        delivery_fee.textContent = molo.textContent;
     } 
     total.textContent = (parseInt(sub_total.textContent.replace(/,/g,"")) + parseInt(delivery_fee.textContent.replace(/,/g,""))).toLocaleString()
 }
